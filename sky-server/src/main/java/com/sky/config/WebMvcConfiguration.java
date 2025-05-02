bf9d04405222213b7464e758e450b85e4ee74865
benjy2346 <92707099+benjy2346@users.noreply.github.com>
2025-05-02 16:40:48 -0600

    用extendMessageConverters进行时间字符串自动格式转换

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,10 +1,15 @@
 package com.sky.config;
 
 import com.sky.interceptor.JwtTokenAdminInterceptor;
+import com.sky.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.realm.JAASCallbackHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -14,6 +19,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 /**
  * 配置类，注册web层相关组件
@@ -64,5 +71,23 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    /**
+     * 扩展SpringMVC  消息转换器, extendMessageConverters是必须要的，不然识别不到
+     * 把后端返回的时间统一重新组织格式，
+     * 本来是 【2003，01，01】， 换成 年-月-日
+     * @param oldConverters
+     *
+     */
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> oldConverters){
+        log.info("注册了客制化的消息转换器");
+//        创建对象转换器
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+
+//        把java对象换成json数据
+        converter.setObjectMapper(new JacksonObjectMapper());
+
+//      把自己的 消息转换器加到容器和框架中自动使用
+        oldConverters.add(0,converter);
     }
 }
